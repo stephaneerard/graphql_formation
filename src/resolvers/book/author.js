3a43efb65d6040d2294370443422736155711f07
Antoine <alepiller@bimandco.com>
2021-07-08 11:24:04 +0200

    test bimobjects

--- src/resolvers/book/author.js
+++ src/resolvers/book/author.js
@@ -1,6 +1,16 @@
-export default () => {
-  return {
-    name: 'test',
-    nbBooks: 1,
-  };
+export default (parent, args, context, info) => {
+  switch (parent.id) {
+    case 1:
+      return {
+        name: 'test',
+        nbBooks: 1,
+      };
+    case 2:
+      return {
+        name: 'test2',
+        nbBooks: 1,
+      };
+    default:
+      break;
+  }
 };
--- src/resolvers/query/books.js
+++ src/resolvers/query/books.js
@@ -1,8 +1,10 @@
-export default () => [
+export default (_, { limit }) => [
   {
+    id: 1,
     title: 'The Awakening',
   },
   {
+    id: 2,
     title: 'City of Glass',
   },
-];
+].slice(0, limit);
